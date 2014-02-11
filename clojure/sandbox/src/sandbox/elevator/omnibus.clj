0cb8c1b8ec3e9782ee76bf380362832bb3f37d68
Jon Newton <jon.newton@gmail.com>
2014-02-10 20:23:26 -0700

    Got omnibus working

---
+++ clojure/sandbox/src/sandbox/elevator/omnibus.clj
@@ -0,0 +1,32 @@
+;; Copied from Laurent Petit's omnibus example:
+;; http://laurentpetit.github.io/blog/2013/11/05/the-clojure-omnibus/
+
+(ns sandbox.elevator.omnibus)
+
+(ns main
+  (:use ring.adapter.jetty
+        compojure.core))
+
+(defn make-omnibus [nb-floors]
+  (let [up   (repeat (dec nb-floors) ["OPEN", "CLOSE", "UP"])
+        down (repeat (dec nb-floors) ["OPEN", "CLOSE", "DOWN"])
+        up-then-down (flatten (concat up down))]
+    (concat ["NOTHING"] (cycle up-then-down))))
+
+(defn tick [elevator] (rest elevator))
+(defn next-command [elevator] (first elevator))
+
+(def nb-floors 20)
+(def cabin (atom (make-omnibus nb-floors)))
+(defn next-command-handler []
+  (let [new-state (swap! cabin tick)]
+    (next-command new-state)))
+
+(defroutes app
+  (GET "/nextCommand" [] (next-command-handler))
+  (GET "*"            [] ""))
+
+(defn -main []
+  (run-jetty app {:port 9090}))
+
+(-main)
