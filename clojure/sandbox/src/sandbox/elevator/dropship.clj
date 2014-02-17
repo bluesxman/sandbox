eaf0227f960d75b1eb12077986d364de1200cc23
Jon Newton <jon.newton@gmail.com>
2014-02-16 23:07:44 -0700

    No async on handler.  Events mod world state
    Removed async from the jetty handler.  May bring it back later.  All
    requests now trigger mods on a world state which can be planned on.
    Planner part just begun.

--- clojure/sandbox/src/sandbox/elevator/dropship.clj
+++ clojure/sandbox/src/sandbox/elevator/dropship.clj
@@ -2,76 +2,114 @@
   (:require [ring.adapter.jetty :refer :all]
             [compojure.core :refer :all]
             [clojure.core.async :as async]))
+(import '(java.util.concurrent Executors))
 
-(def event-queue (async/chan (async/sliding-buffer 50)))
+;;;;;;;;;;;;;;;;;;;;;
+;; Configuration
+;;;;;;;;;;;;;;;;;;;;;
 
-(def request-chan (async/chan (async/sliding-buffer 50)))
+(def num-floors 6)
+(def num-plan-threads 4)
 
-(defn new-world [n-floors]
-  {:calls (vec (repeat n-floors 0))
-   :gotos (vec (repeat n-floors 0))
-   :in-car 0
+
+;;;;;;;;;;;;;;;;;;;;;
+;; Data;
+;;;;;;;;;;;;;;;;;;;;;
+
+(def empty-path {:path [(int (/ num-floors 2))] :cost 0})
+(def best-path (ref default-path))
+
+(def start-world
+  {:calls (vec (repeat num-floors [0 :none])) ;; WARN: we're only recording the last direction!
+   :gotos (vec (repeat num-floors 0))
    :floor 0
    :door :closed})
-
-(defn start-world (new-world 6))
-
 (def world (atom start-world))
+(def world-chan (async/chan))
 
-;; planning needs to read and alter the commands.  handler need to remove and provide next-cmd
-;; best data structure????  Try queue for now
-(def commands (ref (clojure.lang.PersistentQueue/EMPTY)))
+(def open-cmd "OPEN")
+(def close-cmd "CLOSE")
+(def up-cmd "UP")
+(def down-cmd "DOWN")
+
+
+;;;;;;;;;;;;;;;;;;;;;
+;; Functions
+;;;;;;;;;;;;;;;;;;;;;
+
+(defn update-world [f & args]
+  (async/thread (async/>!! world-chan (swap! world f args))))
+
+(defn door-open? [w]
+  (= (w :door) :open))
+
+(defn exiting? [w]
+  (zero? (get-in w [:gotos (w :floor)])))
+
+(defn leave []
+  (swap! world assoc :door :closed)
+  (close-cmd))
+
+(defn at-goal? [w p]
+  (= (w :floor) (first p)))
+
+(defn arrive []
+  (dosync (alter best-path rest))
+  (swap! world assoc :door :open)
+  (open-cmd))
+
+(defn travel [w]
+  (dosync
+   (let [goal (first @best-path)]
+     (if (> goal (w :floor))
+       (do
+         (swap! world update-in [:floor] inc)
+         (up-cmd))
+       (do
+         (swap! world update-in [:floor] dec)
+         (down-cmd))))))
 
 (defn next-cmd []
-  (dosync
-   (let [head (peek @commands)]
-     (case head
-       "OPEN" (swap! world assoc :door :open)
-       "CLOSE" (swap! world assoc :door :closed)
-       "UP" (swap! world (fn [w] (assoc w :floor (inc (w :floor)))))
-       "DOWN" (swap! world (fn [w] (assoc w :floor (dec (w :floor))))))
-     (alter commands pop)
-     head)))
+  (let [w @world          ;; Take a snapshot of the world and reason on it
+        p @best-path]     ;; we know p was made for this world
+    (if (door-open? w)
+      (if (exiting? w)
+        open-cmd ;; just repeat open until all passengers exit
+        (leave)) ;; WARN: not handling special case of no calls AND no gotos
+      (if (at-goal? w p)
+        (arrive)
+        (travel w)))))
 
 (defn reset [qs]
-  (dosync
-   (ref-set commands clojure.lang.PersistentQueue/EMPTY)
-   (reset! world start-world))
-  "elevator reset")
+  (reset! world start-world)
+  (reset! best-path empty-path))
 
-(defn queue-event [e] (async/thread (async/>!! event-queue e)) (str e))
+;; WARN: need to make call and go force the planners to stop and use the new world
+(defn call [qs]
+  (let [[_ fs ds] (re-find #"atFloor=(\d+)&to=(\S+)" qs)
+        floor (read-string fs)
+        dir (if (= "UP" ds) :up :down)
+        update (fn [[cnt d]] (vec (inc cnt) dir))]
+    (swap! world #(update-in % [:calls floor] update))))
 
-(defn queue-call [qs]
-  (let [[_ floor dir] (re-find #"atFloor=(\d+)&to=(\S+)" qs)]
-    (queue-event [:call (read-string floor) (if (= "UP") :up :down)])))
+(defn go [qs]
+  (let [[_ floor-str] (re-find #"floorToGo=(\d+)" qs)
+        floor (read-string floor-str)]
+    (swap! world #(update-in % [:gotos floor] inc))))
 
-(defn queue-go [qs]
-  (let [[_ floor] (re-find #"floorToGo=(\d+)" qs)]
-    (queue-event [:go (read-string floor)])))
+(defn enter []
+  (swap! world #(update-in % [:calls (% :floor) 0] dec)))
 
-(defn queue-entered [] (queue-event [:enter]))
+(defn exit []
+  (swap! world #(update-in % [:gotos (% :floor)] dec)))
 
-(defn queue-exited [] (queue-event [:exit]))
-
-(defroutes route
+(defroutes handle-requests
   (GET "/nextCommand" [] (next-cmd))
-  (GET "/call" {qs :query-string} (queue-call qs))
-  (GET "/go" {qs :query-string} (queue-go qs))
-  (GET "/userHasEntered" [] (queue-entered))
-  (GET "/userHasExited" [] (queue-exited))
-  (GET "/reset" {qs :query-string} (reset qs)))
-
-(defn queue-requests [request]
-  (async/thread (async/>!! request-chan request)))
-
-(defn process-requests [num-threads req-chan]
-  (dotimes [n num-threads]
-    (async/thread
-     (loop [r (async/<!! req-chan)]
-       (if r
-         (do
-           (route r)
-           (recur (async/<!! req-chan))))))))
+  (GET "/call" {qs :query-string} (call qs) "")
+  (GET "/go" {qs :query-string} (go qs) "")
+  (GET "/userHasEntered" [] (enter) "")
+  (GET "/userHasExited" [] (exit) "")
+  (GET "/reset" {qs :query-string} (reset qs) ""))
 
 ;; when nothing to do, goto middle and stay closed
 ;; when empty and called, goto call floor
@@ -83,29 +121,45 @@
 ;;   - Could pay off to pick a longer path that has calls along it that want to go in
 ;;   the right direction
 ;;   - Probably need to wait for the enter/exit events before closing the door
-(defn plan [] ())
+
+(defn submit-world
+  [w tp]
+  ())
+
+(defn plan [wc]
+  (loop [tp (Executors/newFixedThreadPool num-plan-threads)
+         w (async/<!! wc)]
+    (if w
+      (recur (submit-world w tp) (async/<!! wc)))))
+
+
+(defn next-stop [up])
+
+(defn plan-simple [wc]
+  (loop [w (async/<!! wc)]
+    (if w
+      (let [floor (w :floor)
+            above (+ (count-calls :above w) (coumt-gotos :above w))
+            below (+ (count-calls :below w) (count-gotos :below w))]
+        (next-stop (if (> above below)))
+        (recur (submit-world w tp) (async/<!! wc))))))
+
+
 
 ;;;;;;;;;;;;;; Testing
-
-(dosync (dotimes [n 10] (alter commands conj "OPEN" "CLOSE" "UP" "OPEN" "CLOSE" "DOWN")))
 
 (def server (run-jetty handle-requests {:port 9090 :join? false}))
 
 (.stop server)
 
-(async/close! event-queue)
-(def events
-  (loop [events []]
-    (let [e (async/<!! event-queue)]
-      (if e
-        (recur (conj events e))
-        events))))
-(eval events)
+(eval @world)
+(eval @best-path)
+
+
 
 
 
 ;;;;;;;; old omnibus stuff and experiments with ring
-
 
 (defn make-omnibus [nb-floors]
   (let [up   (repeat (dec nb-floors) ["OPEN", "CLOSE", "UP"])
