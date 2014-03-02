05a6e987d5529611931dc72d05321f286658ec50
Jon Newton <jon.newton@gmail.com>
2014-03-01 20:20:06 -0700

    Housekeeping.  Java8+FX to start ui for Life

---
+++ java/src/bluesxman/sandbox/life/LifeView.java
@@ -0,0 +1,52 @@
+package bluesxman.sandbox.life;
+
+import java.util.function.IntSupplier;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.stage.Stage;
+
+public class LifeView extends Application {
+	 
+    public static void main(String[] args) {
+        launch(args);
+    }
+ 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Drawing Operations Test");
+        Group root = new Group();
+        Canvas canvas = new Canvas(300, 250);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+        primaryStage.setScene(new Scene(root));
+
+        Runnable blinkSquare = () -> {
+        	for(int i = 0; i < 1000; i++){
+        		boolean black = i % 2 == 0 ; 
+        		
+        		Platform.runLater( () -> {
+	        		gc.setFill(black ? Color.BLACK : Color.WHITE);
+	        		gc.fillRect(10, 10, 10, 10); 
+	        		primaryStage.show();
+        		});
+        		
+        		try {
+        			Thread.sleep(17);
+        		} catch (InterruptedException e) {
+        			// TODO Auto-generated catch block
+        			e.printStackTrace();
+        		}
+        	}
+        };
+        
+        (new Thread(blinkSquare, "Blink Thread")).start();
+    }
+
+}
